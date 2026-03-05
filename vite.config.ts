9e56432217ea5861453a3f048b2255d95e8971b2
Devansh Arora <hsnaved.reverse@gmail.com>
2026-03-05 05:43:55 +0530

    feat: migrate back to nextjs

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import tailwindcss from "@tailwindcss/vite";
-import vinext from "vinext";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [vinext(), tailwindcss()],
-});
